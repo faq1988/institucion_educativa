815356331743ad773b1e8bb70933cc565e4471fb
faq1988 <facu_carignano@hotmail.com>
2017-10-10 10:26:40 -0300

    Commit inicial (template)

---
+++ application/views/footer.php
@@ -0,0 +1,7 @@
+<footer class="sticky-footer">
+  <div class="container">
+    <div class="text-center">
+      <small>Copyright &copy; Institución educativa 2017</small>
+    </div>
+  </div>
+</footer>
---
+++ application/views/menu.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>Institución educativa</title>
+
+
+
+    <!-- Bootstrap core CSS -->
+    <link href="<?=base_url()?>bootstraptemplate/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom fonts for this template -->
+    <link href="<?=base_url()?>bootstraptemplate/vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+
+    <!-- Plugin CSS -->
+    <link href="<?=base_url()?>bootstraptemplate/vendor/datatables/dataTables.bootstrap4.css" rel="stylesheet">
+
+
+
+    <!-- Custom styles for this template -->
+    <link href="<?=base_url()?>bootstraptemplate/css/sb-admin.css" rel="stylesheet">
+
+
+
+  </head>
+
+  <body class="fixed-nav sticky-footer bg-dark" id="page-top">
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" id="mainNav">
+      <a class="navbar-brand" href="<?=base_url()?>">Institución educativa</a>
+      <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav navbar-sidenav" id="exampleAccordion">
+
+          <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Menu1">
+            <a class="nav-link nav-link-collapse collapsed" data-toggle="collapse" href="#collapseTorneos" data-parent="#exampleAccordion">
+              <i class="fa fa-fw fa-wrench"></i>
+              <span class="nav-link-text">
+                Menu 1</span>
+            </a>
+            <ul class="sidenav-second-level collapse" id="collapseTorneos">
+              <li>
+                <a href="#">Item 1</a>
+              </li>
+              <li>
+                <a href="#">Item 2</a>
+              </li>
+            </ul>
+          </li>
+
+          <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Menu2">
+            <a class="nav-link nav-link-collapse collapsed" data-toggle="collapse" href="#collapseInscripciones" data-parent="#exampleAccordion">
+              <i class="fa fa-fw fa-wrench"></i>
+              <span class="nav-link-text">
+                Menu 2</span>
+            </a>
+            <ul class="sidenav-second-level collapse" id="collapseInscripciones">
+              <li>
+                <a href="#">Item 1</a>
+              </li>
+              <li>
+                <a href="#">Item 2</a>
+              </li>
+            </ul>
+          </li>
+
+          <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Charts">
+            <a class="nav-link" href="#">
+              <i class="fa fa-fw fa-area-chart"></i>
+              <span class="nav-link-text">
+                Menu 3</span>
+            </a>
+          </li>
+          <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Tables">
+            <a class="nav-link" href="#">
+              <i class="fa fa-fw fa-table"></i>
+              <span class="nav-link-text">
+                Menu 4</span>
+            </a>
+          </li>
+
+
+
+        </ul>
+        <ul class="navbar-nav sidenav-toggler">
+          <li class="nav-item">
+            <a class="nav-link text-center" id="sidenavToggler">
+              <i class="fa fa-fw fa-angle-left"></i>
+            </a>
+          </li>
+        </ul>
