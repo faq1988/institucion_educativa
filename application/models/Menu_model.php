00657e17dfcc1442abdb9aee364eac53a100ded5
root <root@localhost.localdomain>
2017-12-05 20:56:33 -0300

    cambio a mayusculas

---
+++ application/models/Menu_model.php
@@ -0,0 +1,41 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Menu_model extends CI_Model{
+
+
+
+public function __construct()
+{
+  parent::__construct();
+}
+
+
+
+
+function crear_menu($data){
+		/*$this->db->insert('evento', array('accion'=>$accion, 'descripcion'=>$descripcion,
+			'id_alumno'=>$id_alumno));
+      */
+	}
+
+
+
+  function obtener_menu(){
+  		$query = $this->db->get('menu_semanal');
+  		if ($query->num_rows() >0 ) return $query;//->result();
+  			}
+
+
+
+
+
+
+
+
+
+
+}
+
+
+?>
